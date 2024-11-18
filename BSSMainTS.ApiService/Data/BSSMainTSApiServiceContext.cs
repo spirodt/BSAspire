b64a73758677b689d43b72b28eb690b1ffd3cd17
Boban Spirovski <spirodt@hotmail.com>
2024-11-18 21:36:43 +0100

    a

--- BSSMainTS.ApiService/Data/BSSMainTSApiServiceContext.cs
+++ BSSMainTS.ApiService/Data/BSSMainTSApiServiceContext.cs
@@ -218,9 +218,7 @@ public partial class BSSMainTSApiServiceContext : DbContext
 
     public virtual DbSet<VwIndexText> VwIndexTexts { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=vlezi.com;Initial Catalog=BSHOME;Persist Security Info=True;User ID=spiro;Password=nirvana;Encrypt=False;Trust Server Certificate=True");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)=> optionsBuilder.UseSqlServer("Server=vlezi.com;Initial Catalog=BSHOME;Persist Security Info=True;User ID=spiro;Password=nirvana;Encrypt=False;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
